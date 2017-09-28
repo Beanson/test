9e3f40ad44d7b6c31f8b42586a9427b2c6858a08
beanson2 <2920248385@qq.com>
2017-09-29 07:23:59 +0800

    dala

--- hello.java
+++ hello.java
@@ -14,18 +14,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 
-import org.monte.media.math.Rational;
-import org.monte.media.Format;
-import org.monte.screenrecorder.ScreenRecorder;
+class hello{
 
-import static org.monte.media.AudioFormatKeys.*;
-import static org.monte.media.VideoFormatKeys.*;
+    private static String a="this is a test for different line";
 
+    public static void main(String[] args) {
 
-public class webdriverdemo {
-    private static ScreenRecorder screenRecorder;
-
-    public static void main(String[] args) throws IOException, AWTException {
         GraphicsConfiguration gconfig = GraphicsEnvironment
                 .getLocalGraphicsEnvironment()
                 .getDefaultScreenDevice()
@@ -42,56 +36,14 @@ public class webdriverdemo {
                 new Format(MediaTypeKey, MediaType.VIDEO,
                         EncodingKey,"black", FrameRateKey, Rational.valueOf(30)), null);
 
-        System.setProperty("webdriver.chrome.driver", "G:\\chromedriver\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
 
-        // Start Capturing the Video
-        screenRecorder.start();
+        System.out.println("hello great darly world!");
+    }
 
-        // Puts an Implicit wait, Will wait for 10 seconds before throwing exception
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void greatThings(String dala){
+        ab=1;
+        ab++;
+        System.out.println(dala+" is not really a better way to do it.");
 
-        // Launch website
-        //driver.navigate().to("http://www.calculator.net/");
-        driver.navigate().to("http://www.baidu.com/");
-
-        // Maximize the browser
-        driver.manage().window().maximize();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Click on Math Calculators
-//        driver.findElement(By.xpath(".//*[@id = 'menu']/div[3]/a")).click();
-//
-//        // Click on Percent Calculators
-//        driver.findElement(By.xpath(".//*[@id = 'menu']/div[4]/div[3]/a")).click();
-//
-//        // Enter value 10 in the first number of the percent Calculator
-//        driver.findElement(By.id("cpar1")).sendKeys("10");
-//
-//        // Enter value 50 in the second number of the percent Calculator
-//        driver.findElement(By.id("cpar2")).sendKeys("50");
-//
-//        // Click Calculate Button
-//        driver.findElement(By.xpath(".//*[@id = 'content']/table/tbody/tr/td[2]/input")).click();
-//
-//        // Get the Result Text based on its xpath
-//        String result =
-//                driver.findElement(By.xpath(".//*[@id = 'content']/p[2]/span/font/b")).getText();
-
-        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshot, new File("D:\\screenshots\\screenshots1.jpg"));
-
-        // Print a Log In message to the screen
-        //System.out.println(" The Result is " + result);
-
-        // Close the Browser.
-        driver.close();
-
-        // Stop the ScreenRecorder
-        screenRecorder.stop();
     }
 }
