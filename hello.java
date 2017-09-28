633e98b4488e9a3c08b61cd6ee1aaa0f818ea594
beanson2 <2920248385@qq.com>
2017-09-28 23:05:47 +0800

    baseline 9/28

---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello{
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+    public void greatThings(String dala){
+        System.out.println(dala+" is really a better way to do it.");
+    }
+}
